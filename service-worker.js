2b46fcb3fceac612f74eeef15c7de204e25e8731
Alessandro <89247430+Alessandro4040@users.noreply.github.com>
2026-03-05 05:53:14 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,16 @@
-const CACHE_NAME = 'orcamento-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.json'
+const CACHE_NAME = 'financas-v5';
+const assets = [
+  './',
+  './index.html',
+  './apps.js',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
