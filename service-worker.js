4019d0602dc3284a02dfae881dc277f00b2cab5c
Alessandro <89247430+Alessandro4040@users.noreply.github.com>
2026-03-10 22:20:22 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,17 @@
-const CACHE_NAME = 'financas-v9';
-const assets = ['./', './index.html', './app.js', './manifest.json'];
+const CACHE_NAME = 'financas-v10';
+const assets = [
+    './',
+    './index.html',
+    './app.js',
+    './manifest.json',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    'https://cdn-icons-png.flaticon.com/512/2341/2341113.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
