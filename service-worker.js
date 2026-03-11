d2558eb028438742ab1991a7075fa45812493865
Alessandro <89247430+Alessandro4040@users.noreply.github.com>
2026-03-10 22:40:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,52 @@
-const CACHE_NAME = 'financas-v10';
+const CACHE_NAME = 'financas-v20';
+
 const assets = [
-    './',
-    './index.html',
-    './app.js',
-    './manifest.json',
-    'https://cdn.jsdelivr.net/npm/chart.js',
-    'https://cdn-icons-png.flaticon.com/512/2341/2341113.png'
+  './',
+  './index.html',
+  './apps.js'
 ];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
+  // Sempre tenta internet primeiro
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, clone);
+        });
+
+        return response;
+
+      })
+      .catch(() => {
+        return caches.match(e.request);
+      })
+  );
+
 });
